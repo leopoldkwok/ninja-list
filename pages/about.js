bd068bee778fa46aaa64d5ab66d20df9020190eb
leopoldkwok <leopoldkwok@gmail.com>
2021-04-06 10:15:58 +0100

    Images & Metadata

--- pages/about.js
+++ pages/about.js
@@ -1,24 +1,32 @@
+import Head from 'next/head';
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis molestias
-        dolore veritatis. Dignissimos aspernatur adipisci alias animi magni hic
-        sit, aut quia quibusdam architecto unde eos cumque illum, aliquam nulla
-        tenetur, esse laborum ratione odio quo ullam magnam? Odio expedita
-        minima sit laborum praesentium? Eligendi quia expedita voluptates ab
-        similique.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis molestias
-        dolore veritatis. Dignissimos aspernatur adipisci alias animi magni hic
-        sit, aut quia quibusdam architecto unde eos cumque illum, aliquam nulla
-        tenetur, esse laborum ratione odio quo ullam magnam? Odio expedita
-        minima sit laborum praesentium? Eligendi quia expedita voluptates ab
-        similique.
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name='keywords' content='ninjas' />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+          molestias dolore veritatis. Dignissimos aspernatur adipisci alias
+          animi magni hic sit, aut quia quibusdam architecto unde eos cumque
+          illum, aliquam nulla tenetur, esse laborum ratione odio quo ullam
+          magnam? Odio expedita minima sit laborum praesentium? Eligendi quia
+          expedita voluptates ab similique.
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+          molestias dolore veritatis. Dignissimos aspernatur adipisci alias
+          animi magni hic sit, aut quia quibusdam architecto unde eos cumque
+          illum, aliquam nulla tenetur, esse laborum ratione odio quo ullam
+          magnam? Odio expedita minima sit laborum praesentium? Eligendi quia
+          expedita voluptates ab similique.
+        </p>
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -6,27 +6,33 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis molestias
-        dolore veritatis. Dignissimos aspernatur adipisci alias animi magni hic
-        sit, aut quia quibusdam architecto unde eos cumque illum, aliquam nulla
-        tenetur, esse laborum ratione odio quo ullam magnam? Odio expedita
-        minima sit laborum praesentium? Eligendi quia expedita voluptates ab
-        similique.
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis molestias
-        dolore veritatis. Dignissimos aspernatur adipisci alias animi magni hic
-        sit, aut quia quibusdam architecto unde eos cumque illum, aliquam nulla
-        tenetur, esse laborum ratione odio quo ullam magnam? Odio expedita
-        minima sit laborum praesentium? Eligendi quia expedita voluptates ab
-        similique.
-      </p>
-      <Link href='/ninjas'>
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name='keywords' content='ninjas' />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+          molestias dolore veritatis. Dignissimos aspernatur adipisci alias
+          animi magni hic sit, aut quia quibusdam architecto unde eos cumque
+          illum, aliquam nulla tenetur, esse laborum ratione odio quo ullam
+          magnam? Odio expedita minima sit laborum praesentium? Eligendi quia
+          expedita voluptates ab similique.
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+          molestias dolore veritatis. Dignissimos aspernatur adipisci alias
+          animi magni hic sit, aut quia quibusdam architecto unde eos cumque
+          illum, aliquam nulla tenetur, esse laborum ratione odio quo ullam
+          magnam? Odio expedita minima sit laborum praesentium? Eligendi quia
+          expedita voluptates ab similique.
+        </p>
+        <Link href='/ninjas'>
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
